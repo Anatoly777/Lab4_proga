5a6f81f1777bb92f6115a8b912bd2b2785182035
Anatoly-LSP-LIZER <tolik.laptev.02@mail.ru>
2020-12-25 22:53:41 +0300

    Add files via upload

---
+++ spons/Sponsor.java
@@ -0,0 +1,56 @@
+package spons;
+
+import expedition.Expedition;
+
+import java.util.Objects;
+
+public final class Sponsor {
+    private final String nameFond;
+    private int money;
+
+    public Sponsor(String name, int cash){
+        this.money = cash;
+        this.nameFond = name;
+        System.out.println("Создан спонсор с именем " + getName() + " и балансом " + money + " тугриков");
+    }
+    public void giveMoney(Expedition exp, int cash){
+        setMoney(getMoney() - cash);
+        exp.setMoney(exp.getMoney() + cash);
+        System.out.println(getName() + " выделяет деньги " + exp.getName() + " в количестве " + cash + " тугриков");
+    }
+
+    public String getName(){
+        return nameFond;
+    }
+
+    public int getMoney(){
+        return money;
+    }
+
+    public void setMoney(int cash){
+        this.money = cash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sponsor sponsors = (Sponsor) o;
+        return Objects.equals(nameFond, sponsors.nameFond);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameFond);
+    }
+
+    @Override
+    public String toString() {
+        return "Sponsors{" +
+                "nameFond='" + nameFond + '\'' +
+                '}';
+    }
+
+
+}
+
