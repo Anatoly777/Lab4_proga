0fd5e89fbe859d88ddbe11e9431c53ab944442f2
Anatoly-LSP-LIZER <tolik.laptev.02@mail.ru>
2020-12-26 09:25:33 +0300

    Add files via upload

---
+++ geographic/City.java
@@ -0,0 +1,48 @@
+package geographic;
+
+import exception.InvalidParameterRuntimeException;
+
+import java.util.Objects;
+
+public final class City extends GeographicObject implements GeoObject{
+    private String name;
+
+    public City(String name) {
+        if (name == null){
+            throw new InvalidParameterRuntimeException();
+        }
+        setTypeObject("Город");
+        setNameObject(name);
+        setName(getTypeObject() + " " + getNameObject());
+        System.out.println("Создан географический объект: " + getName());
+    }
+
+    public void setName(String nm){
+        this.name = nm;
+    }
+
+    @Override
+    public String toString() {
+        return "City{" +
+                "Name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        City city = (City) o;
+        return Objects.equals(name, city.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), name);
+    }
+}
---
+++ geographic/GeoObject.java
@@ -0,0 +1,6 @@
+package geographic;
+
+public interface GeoObject {
+    String getName();
+    void setName(String name);
+}
